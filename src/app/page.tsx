2c799d1d1a1ffd7047b1497d02595ec2ffe2c4d0
Jishnu <jishnukhargharia11@gmail.com>
2025-03-19 22:08:05 +0530

    add skills

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import About from "@/components/About";
+import Skills from "@/components/Skills";
 
 export default function Home() {
   return (
     <>
-      <main className="bg-[#0b0c10] py-32">
+      <main className="bg-[#0b0c10] text-white py-32">
         <About />
+        <Skills />
         <div>
           home page
         </div>
---
+++ src/components/Skills.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Skills = () => {
+  return (
+    <>
+        <div>
+            5fe9df
+        </div>
+    </>
+  )
+}
+
+export default Skills
