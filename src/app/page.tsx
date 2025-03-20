5e624d3b0177fc3da3e9ceb64b28952c17c99b0a
Jishnu <jishnukhargharia11@gmail.com>
2025-03-20 13:40:33 +0530

    add contact page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import About from "@/components/About";
 import Skills from "@/components/Skills";
+import Contact from "@/components/Contact";
 
 export default function Home() {
   return (
@@ -8,9 +9,7 @@ export default function Home() {
       <main className="bg-[#0b0c10] text-white py-32">
         <About />
         <Skills />
-        <div>
-          home page
-        </div>
+        <Contact />
       </main>
     </>
   );
