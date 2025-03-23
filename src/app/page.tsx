ef1fb7a04fdb65a46d59ccaf2c5888e98f309ae7
Jishnu <jishnukhargharia11@gmail.com>
2025-03-23 19:21:34 +0530

    add aboutme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,14 @@ import About from "@/components/About";
 import Skills from "@/components/Skills";
 import Contact from "@/components/Contact";
 import Footer from "@/components/Footer";
+import Aboutme from "@/components/Aboutme";
 
 export default function Home() {
   return (
     <>
       <main className="bg-[#0b0c10] text-white pt-32 pb-10">
         <About />
+        <Aboutme />
         <Skills />
         <Contact />
         <Footer />
