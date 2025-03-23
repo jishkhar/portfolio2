9aa61c270dcc841ad570657a05145fd32f0fe0fc
Jishnu <jishnukhargharia11@gmail.com>
2025-03-23 21:35:58 +0530

    modify contact

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,5 +1,8 @@
 "use client";
 import React, { useState } from "react";
+import { FiGithub, FiLinkedin, FiTwitter } from 'react-icons/fi';
+import { MdOutlineEmail } from 'react-icons/md';
+import Link from "next/link";
 
 const Contact = () => {
   const [submitted, setSubmitted] = useState(false);
@@ -8,7 +11,7 @@ const Contact = () => {
     e.preventDefault();
     const form = e.currentTarget;
     const formData = new FormData(form);
-    
+
     const response = await fetch("https://api.web3forms.com/submit", {
       method: "POST",
       headers: {
@@ -32,50 +35,67 @@ const Contact = () => {
   }
 
   return (
-    <div className="h-screen flex flex-col items-center justify-center p-6">
-      <div className="bg-[#1f2833] shadow-lg rounded-2xl p-10 w-[30vw] h-[67vh]">
-        <h2 className="text-[3rem] font-semibold text-center mb-10">Contact Us</h2>
-        <form className="space-y-4" onSubmit={handleSubmit}>
-          <div>
-            <label className="block text-[1.3rem]">Name</label>
-            <input
-              type="text"
-              name="name"
-              className="text-black w-full p-2 border-4 rounded-lg focus:outline-none focus:border-[#2fffe5]"
-              placeholder="Your Name"
-            />
+    <div className="h-screen flex items-center justify-center p-6">
+      <div className="flex shadow-lg rounded-2xl w-[70vw] h-[67vh] overflow-hidden">
+        {/* Left Div */}
+        <div className="flex flex-col items-center mt-20 pr-16 w-1/2 bg-[#0b0c10] text-white text-2xl font-semibold gap-y-4">
+          <div className="text-[#54ccc4] text-center">Liked my work&#63;</div>
+          <div className="text-[2.2rem] text-center">Reach Out to Me!</div>
+          <p className="text-center text-[#C2C2C3] pt-4">
+            I am always looking for new opportunities to work with professionals and companies. I would also like to contribute to open-source projects providing real-world value.
+          </p>
+          <div id="icons" className="p-5 mt-10 text-[1.8rem] border rounded-lg border-[#54ccc4] flex justify-center items-center cursor-pointer transition-all duration-300 hover:text-[#5fe9df] hover:scale-105">
+            <Link href="mailto:jishnukhargharia11@gmail.com" className="flex items-center">
+              <h1>Contact Me : &nbsp;</h1>
+              <MdOutlineEmail className="text-4xl" />
+            </Link>
           </div>
-          <div>
-            <label className="block text-[1.3rem] mt-7">Email</label>
-            <input
-              type="email"
-              name="email"
-              className="text-black w-full p-2 border-4 rounded-lg focus:outline-none focus:border-[#2fffe5]"
-              placeholder="Your Email"
-            />
-          </div>
-          <div>
-            <label className="block text-[1.3rem] mt-7">Message</label>
-            <textarea
-              name="message"
-              className="text-black w-full p-2 border-4 rounded-lg focus:outline-none focus:border-[#2fffe5]"
-              rows={4}
-              placeholder="Your Message"
-            ></textarea>
-          </div>
-          <button
-            type="submit"
-            className="w-full h-14 bg-[#5ee6dd] text-[#0b0c10] p-2 rounded-lg hover:bg-[#2fffe5] transition mt-10"
-          >
-            Send Message
-          </button>
-        </form>
-      </div>
-      {submitted && (
-        <div className="mt-4 bg-green-500 text-white p-3 rounded-lg text-center w-[30vw]">
-          Submitted Successfully
         </div>
-      )}
+        {/* Right Div */}
+        <div className="w-1/2 p-10 border rounded-lg border-[#54ccc4]">
+          <h2 className="text-[2.5rem] font-semibold text-center mb-6">Contact Us</h2>
+          <form className="space-y-4" onSubmit={handleSubmit}>
+            <div>
+              <label className="block text-[1.3rem]">Name</label>
+              <input
+                type="text"
+                name="name"
+                className="text-black w-full p-2 border-4 rounded-lg focus:outline-none focus:border-[#2fffe5]"
+                placeholder="Your Name"
+              />
+            </div>
+            <div>
+              <label className="block text-[1.3rem] mt-4">Email</label>
+              <input
+                type="email"
+                name="email"
+                className="text-black w-full p-2 border-4 rounded-lg focus:outline-none focus:border-[#2fffe5]"
+                placeholder="Your Email"
+              />
+            </div>
+            <div>
+              <label className="block text-[1.3rem] mt-4">Message</label>
+              <textarea
+                name="message"
+                className="text-black w-full p-2 border-4 rounded-lg focus:outline-none focus:border-[#2fffe5]"
+                rows={4}
+                placeholder="Your Message"
+              ></textarea>
+            </div>
+            <button
+              type="submit"
+              className="w-full h-14 bg-[#5ee6dd] text-[#0b0c10] p-2 rounded-lg hover:bg-[#2fffe5] transition mt-6"
+            >
+              Send Message
+            </button>
+          </form>
+          {submitted && (
+            <div className="mt-4 bg-green-500 text-white p-3 rounded-lg text-center">
+              Submitted Successfully
+            </div>
+          )}
+        </div>
+      </div>
     </div>
   );
 };
